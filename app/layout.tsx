904338d61c8aea698a0530fe81bf3b1ae7c3101f
Vansh <96967090+uvansh@users.noreply.github.com>
2024-12-08 22:33:44 +0530

    Added user log in and Finalized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
